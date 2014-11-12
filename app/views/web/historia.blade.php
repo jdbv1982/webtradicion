f83cd5ee4190d34619ebbddcd5ca3f085c58e997
jdbv1982 <jdbardav@gmail.com>
2014-11-12 16:57:28 -0600

    Se inicia la creacion de la pagina de historia

--- app/views/web/historia.blade.php
+++ app/views/web/historia.blade.php
@@ -1,5 +1,23 @@
 @extends('web/menu')
 
 @section('sub-content')
-    <h1>Historia</h1>
+	<div class="Historia">
+		<div class="Historia-arbol"></div>
+		<div class="Historia-texto">
+			<div class="Historia-titulo">	LA TRADICIÓN</div>
+			<div class="Historia-contenido">
+				<p>Un domingo de plaza en la ciudad de Tlacolula de Matamoros por el año 1880, la que fuera esposa de Don Rómulo Chagoya encontró a un niño huérfano llorando en el atrio de la iglesia. Sin lograr conocer su procedencia decidió adoptarlo y bautizarlo con el nombre de ‘Jorge Chagoya Sánchez ’.</p>
+				<p>El carácter indómito e inquieto de Jorge lo llevó a incursionar en actividades diferentes al promedio de la época y fue así como comenzó el linaje mezcalero que lleva hoy por nombre Tradición Chagoya.</p>
+			</div>
+			<div class="Historia-menu">
+				<ul>
+					<li>I</li>
+					<li>II</li>
+					<li>III</li>
+					<li>IV</li>
+					<li>V</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 @stop
