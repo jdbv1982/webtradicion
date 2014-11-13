4ff0d9b79a7adeede2b592bd9c7d9b4453cc2db0
jdbv1982 <jdbardav@gmail.com>
2014-11-13 17:00:44 -0600

    se crea los links de las redes sociales

--- app/views/layouts/header.blade.php
+++ app/views/layouts/header.blade.php
@@ -1,2 +1,27 @@
+<div class="Redes">
+	<div class="Redes-iconos">
+		<a href="https://www.facebook.com/pages/Tradicion-Chagoya/529369563767094?fref=ts" target="_blank">
+			<div class="Redes-facebook icono"></div>
+		</a>
+		<a href="https://twitter.com/MezcalChagoya" target="_blank">
+			<div class="Redes-twitter icono"></div>
+		</a>
+		<a href="http://www.linkedin.com/company/bebidas-excelentes-s-a-de-c-v-?trk=biz-companies-cym" target="_blank">
+			<div class="Redes-linkedin icono"></div>
+		</a>
+		<a href="">
+			<div class="Redes-mail-suscripcion icono"></div>
+		</a>
+	</div>
+	<div class="Redes-idiomas">
+		<a href="">
+			<div class="link-idioma">Español</div>
+		</a>
+		<a href="">
+			<div class="link-idioma">Ingles</div>
+		</a>
+	</div>
+
+</div>
 <div class="web-div-header">
 </div>
